0a675a5e1ad5fec72dc2a71a185584baa1b70fa4
rakoon33 <phamnam7892@gmail.com>
2025-03-24 20:36:44 +0700

    Initial Commit

---
+++ GroceryGo/GroceryGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryGoApp.swift
+//  GroceryGo
+//
+//  Created by Phạm Văn Nam on 24/3/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroceryGoUITests/GroceryGoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  GroceryGoUITestsLaunchTests.swift
+//  GroceryGoUITests
+//
+//  Created by Phạm Văn Nam on 24/3/25.
+//
+
+import XCTest
+
+final class GroceryGoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
