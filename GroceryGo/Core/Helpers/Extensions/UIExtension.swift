b170ad78c4dab41eacb4d7bed5e3cbe117f445bf
rakoon33 <phamnam7892@gmail.com>
2025-08-23 00:44:05 +0700

    add RoundCorner for View

--- GroceryGo/Core/Helpers/Extensions/UIExtension.swift
+++ GroceryGo/Core/Helpers/Extensions/UIExtension.swift
@@ -149,3 +149,21 @@ struct ShowButton: ViewModifier {
         }
     }
 }
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corner: UIRectCorner) -> some View {
+        clipShape(RoundCorner(radius: radius, corners: corner))
+    }
+}
+
+
+struct RoundCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect ,byRoundingCorners: corners ,cornerRadii: CGSize(width: radius, height: radius))
+        
+        return Path(path.cgPath)
+    }
+}
