388efcf048379bf96e9de49bec32ae5ed35a6309
rakoon33 <phamnam7892@gmail.com>
2025-08-23 00:43:09 +0700

    add NutritionModel

--- GroceryGo/Model/NutritionModel.swift
+++ GroceryGo/Model/NutritionModel.swift
@@ -5,14 +5,28 @@
 //  Created by Phạm Văn Nam on 22/8/25.
 //
 
+import Foundation
 import SwiftUI
 
-struct NutritionModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
-#Preview {
-    NutritionModel()
+struct NutritionModel: Codable, Identifiable, Equatable {
+    let id: Int
+    let nutritionName: String
+    let nutritionValue: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "nutrition_id"
+        case nutritionName = "nutrition_name"
+        case nutritionValue = "nutrition_value"
+    }
+    
+    init(id: Int = 0, nutritionName: String = "", nutritionValue: String = "") {
+           self.id = id
+           self.nutritionName = nutritionName
+           self.nutritionValue = nutritionValue
+       }
+    
+    static func == (lhs: NutritionModel, rhs: NutritionModel) -> Bool {
+        lhs.id == rhs.id
+    }
 }
