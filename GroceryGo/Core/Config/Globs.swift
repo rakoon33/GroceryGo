b04f42cb3827c3cc3740a5f790ecb6a1c5c71696
rakoon33 <phamnam7892@gmail.com>
2025-08-23 00:44:45 +0700

    add ProductDetailView

--- GroceryGo/Core/Config/Globs.swift
+++ GroceryGo/Core/Config/Globs.swift
@@ -18,6 +18,7 @@ struct Globs {
     static let SV_LOGIN = BASE_URL + "login"
     static let SV_SIGN_UP = BASE_URL + "sign_up"
     static let SV_HOME = BASE_URL + "home"
+    static let SV_PRODUCT_DETAIL = BASE_URL + "product_detail"
 }
 
 struct KKey {
