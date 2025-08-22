5ced2843548b4cce00bc29aba683857fedac1a14
rakoon33 <phamnam7892@gmail.com>
2025-08-22 12:29:17 +0700

    add home view and homeview model

--- GroceryGo/View/Home/HomeView.swift
+++ GroceryGo/View/Home/HomeView.swift
@@ -8,11 +8,116 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @StateObject var homeVM = HomeViewModel.shared
+    
     var body: some View {
-        VStack {
-            Text("Home view")
-        }
+        
+        ZStack {
+            ScrollView {
+                VStack {
+                    Image("color_logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 25)
+                    
+                    HStack {
+                        Image("location")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 16, height: 16)
+                        
+                        Text("Viet Nam, HCM")
+                            .font(.customfont(.semibold, fontSize: 18))
+                            .foregroundStyle(.darkGray)
+                    }
+                    
+                    SearchTextField(placeholder: "Search store", txt: $homeVM.txtSearch)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+                }
+                .padding(.top, .topInsets)
+                
+                
+                Image("banner_top")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 115)
+                    .padding(.horizontal, 20)
+                
+                SectionTitleAll(title: "Exclusive offer", titleAll: "See all") {
+                    
+                }
+                .padding(.horizontal, 20)
+   
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: 15) {
+                        ForEach(homeVM.offerArr, id: \.id) { pObj in
+                            ProductCell(pObj: pObj) {
+                                
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 4)
+                }
 
+                
+                SectionTitleAll(title: "Best selling", titleAll: "See all") {
+                    
+                }
+                .padding(.horizontal, 20)
+    
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: 15) {
+                        ForEach(homeVM.bestArr, id: \.id) { pObj in
+                            ProductCell(pObj: pObj) {
+                                
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 4)
+                }
+                
+                SectionTitleAll(title: "Groceries", titleAll: "See all") {
+                    
+                }
+                .padding(.horizontal, 20)
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: 15) {
+                        ForEach(0...5, id: \.self) { index in
+                            CategoryCell(color: Color(hex: "F8A44C")) {
+                                
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 4)
+                }
+                .padding(.bottom, 8)
+                
+//                ScrollView(.horizontal, showsIndicators: false) {
+//                    LazyHStack(spacing: 15) {
+//                        ForEach(0...5, id: \.self) { index in
+//                            ProductCell() {
+//                                
+//                            }
+//                        }
+//                    }
+//                    .padding(.horizontal, 20)
+//                    .padding(.vertical, 4)
+//                }
+//                .padding(.bottom, 15)
+            }
+        }
+        .ignoresSafeArea()
+        .alert(isPresented: $homeVM.showError) {
+            
+            Alert(title: Text(Globs.AppName), message: Text(homeVM.errorMessage), dismissButton: .default(Text("ok_button".localized)))
+        }
             
     }
     
--- GroceryGo/ViewModel/HomeViewModel.swift
+++ GroceryGo/ViewModel/HomeViewModel.swift
@@ -11,7 +11,43 @@ final class HomeViewModel: ObservableObject {
   
     static var shared: HomeViewModel = HomeViewModel()
     
-    @Published var selectedTab: MainTab = .shop
+    private let homeService: HomeServiceProtocol
     
+    @Published var selectedTab: MainTab = .shop
+    @Published var txtSearch: String = ""
+    
+    @Published var isLoading: Bool = false
+    @Published var showError = false
+    @Published var errorMessage: String = ""
+    
+    @Published var offerArr: [ProductModel] = []
+    @Published var bestArr: [ProductModel] = []
+    @Published var listArr: [ProductModel] = []
+    @Published var typeArr: [TypeModel] = []
+    
+    init(homeService: HomeServiceProtocol = HomeService()) {
+        
+        self.homeService = homeService
+        
+        isLoading = true
+        homeService.fetchHomeData() { [weak self] result in
+            DispatchQueue.main.async {
+                guard let self = self else { return }
+                switch result {
+                case .success(let data):
+                    self.offerArr = data.offers
+                    self.bestArr = data.bests
+                    self.listArr = data.list
+                    self.typeArr = data.types
+                    self.isLoading = false
+                case .failure(let error):
+                    self.errorMessage = error.errorMessage
+                    self.showError = true
+                    self.isLoading = false
+                }
+            }
+        }
+        
+    }
 }
 
